8996be09e77bed8c3fab52bb26d44a64a72d7399
Marc Cavigli <mcavigli@gmail.com>
2020-08-11 18:17:03 -0700

    Updated Grabber script
    This script is an abandoned effort and didn't end up getting used in the final project. I still have it becuase there's some good stuff in there that might be useful for later.

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// WIP: This script isn't currently used in the project.
+/// This script allows the user to distance grab objects.
+/// </summary>
 public class Grabber : MonoBehaviour
 {
+    /// <summary>
+    /// Refers to the hand of either the right or left controller depending
+    /// on which the user is using to pick up an object.
+    /// </summary>  
     public GameObject collidingObject;
+
+    /// <summary>
+    /// WIP
+    /// </summary>  
     public GameObject objectInHand;
+
+    /// <summary>
+    /// Method that occurs when a player picks up an object.
+    /// <args>Collider other refers to the controller the player is using to pick up
+    /// the object. </args>
+    /// </summary>  
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.GetComponent<GameObject>())
                 collidingObject = other.gameObject;
     }
+
+    /// <summary>
+    /// Method that occurs when a player drops an object.
+    /// <args> Collider other refers to the controller the player is using to drop
+    /// the object. </args>
+    /// </summary>   
     public void OnTriggerExit(Collider other)
     {
         collidingObject = null;
