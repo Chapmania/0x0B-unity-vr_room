a983ee39767322d8e19a8a05f4e67080bec7528e
Marc Cavigli <mcavigli@gmail.com>
2020-08-11 17:22:46 -0700

    Added comments and cleaned up HighlightObject script
    Added XML documentation to the variables and methods in the HighlightObject script.
    
    This script detects when a player is in range of an interactable object. If they are in range and they aim a controller at the object, the object will change color to give feedback to the player that alerts them that they're able to pick up the object.

--- Assets/Scripts/HighlightObject.cs
+++ Assets/Scripts/HighlightObject.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script provides feedback to the player of what objects they are able to pick up.
+/// Interactable objects will change color when the player is close enough to them and aiming
+/// their controller at them.
+/// </summary>
 public class HighlightObject : MonoBehaviour
 {
+    /// <summary>
+    /// The trigger to detect if an object is currently highlighted or not.
+    /// </summary>
     Renderer touchCurrentRenderer;
+
+    /// <summary>
+    /// The normal, original color of the interactable object.
+    /// </summary>
     Color touchOriginalColor;
+
+    /// <summary>
+    /// The color the interactable object changes to when a controller is aimed at it
+    /// and the player is close enough.
+    /// </summary>
     public Color touchHighlightColor;
 
+/// <summary>
+/// Method that is triggered when a controller highlights an interactable object.
+/// <args> Collider other refers to the player's controller. </args>
+/// </summary>
     void OnTriggerStay(Collider other)
     {
         if (touchCurrentRenderer == null && other.tag == "Grabbable")
@@ -20,6 +41,11 @@ public class HighlightObject : MonoBehaviour
             }
         }
     }
+
+/// <summary>
+/// Method that is triggered when a controller is no longer aimed at an interactable object.
+/// <args> Collider other refers to the player's controller. </args>
+/// </summary>
     void OnTriggerExit(Collider other)
     {
         touchCurrentRenderer.material.color = touchOriginalColor;
