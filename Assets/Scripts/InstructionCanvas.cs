07d27203661605a8437888adc35dce2c3f6ae8ed
Marc Cavigli <mcavigli@gmail.com>
2020-08-11 18:20:36 -0700

    Cleaned up InstructionCanvas script and added comments
    Cleaned up the script for the togglable canvas that displays how to play the game to the player. Added XML documentation as well.

--- Assets/Scripts/InstructionCanvas.cs
+++ Assets/Scripts/InstructionCanvas.cs
@@ -2,26 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script shows the player how to interact with the world and what actions
+/// are mapped to which buttons. It is togglable.
+/// It's also a WIP. I'd like to spend some more time on the UI to make it more user
+/// friendly and elegant.
+/// </summary>
 public class InstructionCanvas : MonoBehaviour
 {
+    /// <summary>
+    /// The Canvas that shows the instructions on how to play the game.
+    /// </summary>  
     public GameObject InstructionsPanel;
 
+    /// <summary>
+    /// Method that constantly checks to see if the instructions canvas is being toggled
+    /// on or off.
+    /// </summary>  
     void FixedUpdate()
     {
         if (OVRInput.GetDown(OVRInput.Button.Two))
-        {
             ToggleInstructions();
-        }
     }
+
+    /// <summary>
+    /// Method that does the actual toggling of the instructions canvas.
+    /// </summary>  
     void ToggleInstructions()
     {
         if (InstructionsPanel.activeSelf)
-        {
             InstructionsPanel.SetActive(false);
-        }
         else
-        {
             InstructionsPanel.SetActive(true);
-        }
     }
 }
