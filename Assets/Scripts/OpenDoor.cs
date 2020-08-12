f875545025b46b98bc9cd9a2f0a9d346ba30fcd0
Marc Cavigli <mcavigli@gmail.com>
2020-08-11 18:26:33 -0700

    Added comments and cleaned up OpenDoor script
    Added XML documentation for the script that animates the door swinging open and closed once the player unlocks the door with the console then interacts with the door.

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -2,18 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script allows the user to open the door once the console has been unlocked.
+/// </summary>  
 public class OpenDoor : MonoBehaviour
 {
+    /// <summary>
+    /// The Right Controller.
+    /// </summary>  
     public Collider Rcontroller;
+
+    /// <summary>
+    /// The Left Controller.
+    /// </summary> 
     public Collider Lcontroller;
+
+    /// <summary>
+    /// Animator for the door swinging open.
+    /// </summary> 
     Animator anim;
+
+    /// <summary>
+    /// The door GameObject.
+    /// </summary> 
     Collider door;
+
+    /// <summary>
+    /// Instantiates the variables.
+    /// </summary> 
     void Awake()
     {
         anim = GetComponent<Animator>();
         door = GetComponent<BoxCollider>();
     }
 
+    /// <summary>
+    /// Constantly checks to see if either the bounds of the Right or Left controller intersect with
+    /// the bounds of the door. If they do and the user presses the correct button, the door will
+    /// swing open (if it has been unlocked with the console).
+    /// </summary> 
     void FixedUpdate()
     {
         if (Rcontroller.bounds.Intersects(door.bounds) && OVRInput.GetDown(OVRInput.Button.One))
@@ -32,10 +59,18 @@ public class OpenDoor : MonoBehaviour
         }
 
     }
+
+    /// <summary>
+    /// Method to have the door animate open.
+    /// </summary> 
     void Open()
     {
         anim.SetBool("open_door", true);
     }
+
+    /// <summary>
+    /// Method to have the door animate closed.
+    /// </summary> 
     void Close()
     {
         anim.SetBool("open_door", false);
