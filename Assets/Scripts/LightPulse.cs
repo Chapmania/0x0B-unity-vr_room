1656b831a80683f2d65fc67b54ba1f6aceab5d6b
Marc Cavigli <mcavigli@gmail.com>
2020-08-11 18:22:50 -0700

    Added documentation to LightPulse script
    This script was abandoned as I didn't have enough time to figure out how to get it to work the way I wanted. I would like to come back to it in the future because I think it'd be a neat and immersive idea.
    
    Added XML documentation.

--- Assets/Scripts/LightPulse.cs
+++ Assets/Scripts/LightPulse.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// WIP
+/// Abandoned script that was going to pulse the light of the button.
+/// </summary>
 public class LightPulse : MonoBehaviour
 {
+    /// <summary>
+    /// The light of the button.
+    /// </summary>  
     Light buttonLight;
 
+    /// <summary>
+    /// Instantiates the variable.
+    /// </summary>  
     void Awake()
     {
         buttonLight = GetComponent<Light>();
     }
+
+    /// <summary>
+    /// The code that would have caused the light to pulsate.
+    /// </summary>  
     void FixedUpdate()
     {
         buttonLight.intensity = Mathf.PingPong(Time.deltaTime, 6);
